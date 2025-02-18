8caa610d5fe2de2bdfb32fe00493120e3deda488
Rodrigo Murta <rodrigommurta@gmail.com>
2025-02-18 03:32:04 -0300

    feat(Data): NetworkAdapter

---
+++ app/src/main/java/com/picpay/desafio/android/data/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.picpay.desafio.android.data.utils
+
+object Constants {
+    internal const val BASE_URL = "https://609a908e0f5a13001721b74e.mockapi.io/picpay/api/"
+    internal const val OK_HTTP = "Ok Http"
+}
---
+++ app/src/main/java/com/picpay/desafio/android/data/utils/NetworkAdapter.kt
@@ -0,0 +1,41 @@
+package com.picpay.desafio.android.data.utils
+
+import com.picpay.desafio.android.domain.utils.State
+import com.picpay.desafio.android.domain.utils.toErrorInformation
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+
+inline fun <Result, Request> networkAdapter(
+    crossinline query: () -> Flow<Result>,
+    crossinline get: suspend () -> Request,
+    crossinline saveGetResult: suspend (Request) -> Unit,
+): Flow<State<Result>> = flow {
+    var data = query().first()
+
+    try {
+        saveGetResult(get())
+        data = query().first()
+
+    } catch (error: Exception) {
+        if (data == emptyList<Result>() || data != null) {
+            emit(
+                State.ErrorWithCache(
+                    data,
+                    error.toErrorInformation(
+                        "Não foi possível conectar ao servidor. Exibindo dados em cache."
+                    ),
+                )
+            )
+        } else {
+            emit(
+                State.Error(
+                    error = error.toErrorInformation(
+                        "Não foi possível conectar ao servidor."
+                    ),
+                )
+            )
+        }
+    }
+    emit(State.Success(data))
+}
